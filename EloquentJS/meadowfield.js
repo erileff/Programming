b25160626e91ab6f256ff18f85bbb7176632d837
Erica Leff <ericaleff@gmail.com>
2020-05-28 20:43:33 -0400

    meadowfield

--- EloquentJS/meadowfield.js
+++ EloquentJS/meadowfield.js
@@ -4,19 +4,19 @@ const roads = [
 ];
 
 function buildGraph(edges) {
-    /* Creates a null (not empty) object to hold a collection of places in the village with roads between them for the robot to move through. Question: is there a reason the object has to be null and not empty? */
+    /** Creates a null (not empty) object to hold a collection of places in the village with roads between them for the robot to move through. Question: is there a reason the object has to be null and not empty? */
     let graph = Object.create(null);
 
-    /* addEdge adds a list of places to graph and shows every other place that can be reached from that place */
+    /** addEdge adds a list of places to graph and shows every other place that can be reached from that place */
     function addEdge(from, to) { 
-        /* If it's not a 'from' place, it's set as a 'to' array. If it is a 'from' place, it's added to the relevant 'to' array. */
+        /** If it's not a 'from' place, it's set as a 'to' array. If it is a 'from' place, it's added to the relevant 'to' array. */
         if (graph[from] == null) {
             graph[from] = [to];
         } else {
             graph[from].push(to);
         }
     }
-    /* This splits the roads array's items and then adds edges between the places. */
+    /** This splits the roads array's items and then adds edges between the places. */
     for (let [from, to] of edges.map(r => r.split("-"))) {
         addEdge(from, to);
         addEdge(to, from);
@@ -27,16 +27,17 @@ function buildGraph(edges) {
 const roadGraph = buildGraph(roads);
 
 class VillageState {
-    
     constructor(place, parcels) {
         this.place = place;
         this.parcels = parcels;
     }
     
+    /** move takes a parameter, the robot's destination. It checks if there's a road going from the robot's current place (this.place) to the destination. */
     move(destination) {
         if (!roadGraph[this.place].includes(destination)) {
             return this;
         } else {
+            /** If there is a road from this.place to destination, parcels that the robot still needs to deliver are put into an array (parcels). */
             let parcels = this.parcels.map(p => {
                 if (p.place != this.place) return p;
                 return {place: destination, address: p.address};
@@ -44,4 +45,81 @@ class VillageState {
             return new VillageState(destination, parcels);
         }
     }
+}
+
+/* let first = new VillageState("Post Office", [{place: "Post Office", address: "Alice's House"}]);
+let next = first.move("Alice's House");
+
+console.log(first.place); // Post Office
+console.log(first.parcels); // needs to go to Alice's House
+console.log(next.place); // Alice's House
+console.log(next.parcels); // parcel was delivered, so returns [] */
+
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Post Office", parcels);
+};
+
+const mailRoute = ["Alice's House", "Cabin", "Alice's House", "Bob's House", "Town Hall", "Daria's House", "Ernie's House", "Grete's House", "Shop", "Grete's House", "Farm", "Marketplace", "Post Office"];
+
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+}
+
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
 }
